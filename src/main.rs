77719f606e5c49da9ae56288faf45cc12bff2c2d
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-05 13:17:22 +0530

    created struct of expense and added add_expense functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// Structure of an expense
+struct Expense{
+    name: String,
+    date: String,
+    amount: f64
+}
+
+// Create a vector of expenses
+
+//add expense function 
+fn add_expense(name: String, date: String, amount: f64, expenses_vector: &mut Vec<Expense>){
+    expenses_vector.push(Expense { name: String::from(name), date: String::from(date), amount: amount });
+
+}
+
+//delete expense function
+
+//update expense function
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut expenses: Vec<Expense> = Vec::new();
+
+    add_expense(String::from("test"), String::from("test"), 60.00, &mut expenses);
+
+    for expense in &expenses{
+        println!("{} {} {}",expense.name,expense.date,expense.amount)
+    }
+    println!()
 }
