4c9a1241e02c59274cf1d4df67bd40ba24571ae1
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-06 11:27:17 +0530

    basic update function done

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,18 @@ fn delete_expense(id: i32, expenses_vector: &mut Vec<Expense>){
 
 //update expense function
 
+fn update_expense(id: i32, updated_name: String, updated_date: String, updated_amount: f64, expenses_vector: &mut Vec<Expense>){
+    for expense in expenses_vector.iter_mut(){
+        if id == expense.id {
+            expense.name = updated_name;
+            expense.date = updated_date;
+            expense.amount = updated_amount;
+            return;
+        }
+    }
+    println!("Element not found")
+}
+
 
 fn main() {
     let mut expenses: Vec<Expense> = Vec::new();
@@ -43,6 +55,10 @@ fn main() {
     add_expense(4, String::from("test"), String::from("test"), 60.00, &mut expenses);
 
     delete_expense(5, &mut expenses);
+
+    update_expense(1, String::from("test1"), String::from("test1"), 80.00, &mut expenses);
+
+
 
     for expense in &expenses{
         println!("{} {} {} {}",expense.id,expense.name,expense.date,expense.amount)
