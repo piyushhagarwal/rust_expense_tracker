aa79edb0779363dc5f9c436d504d80c9e95691de
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-06 10:57:42 +0530

    basic delete function done

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 // Structure of an expense
 struct Expense{
+    id:i32,
     name: String,
     date: String,
     amount: f64
 }
 
-// Create a vector of expenses
-
 //add expense function 
-fn add_expense(name: String, date: String, amount: f64, expenses_vector: &mut Vec<Expense>){
-    expenses_vector.push(Expense { name: String::from(name), date: String::from(date), amount: amount });
+fn add_expense(id: i32, name: String, date: String, amount: f64, expenses_vector: &mut Vec<Expense>){
+    expenses_vector.push(Expense {id:id, name: String::from(name), date: String::from(date), amount: amount });
 
 }
 
 //delete expense function
+fn delete_expense(id: i32, expenses_vector: &mut Vec<Expense>){
+    let mut index = 0;
+    for element in expenses_vector.iter(){
+        if element.id == id {
+            break;
+        }
+        else{
+            index += 1;
+        }
+    }
+    if index < expenses_vector.len() {
+        expenses_vector.remove(index);
+    }
+    else{
+        println!("expense not found");
+    }
+}
 
 //update expense function
 
@@ -21,10 +37,15 @@ fn add_expense(name: String, date: String, amount: f64, expenses_vector: &mut Ve
 fn main() {
     let mut expenses: Vec<Expense> = Vec::new();
 
-    add_expense(String::from("test"), String::from("test"), 60.00, &mut expenses);
+    add_expense(1, String::from("test"), String::from("test"), 60.00, &mut expenses);
+    add_expense(2, String::from("test"), String::from("test"), 60.00, &mut expenses);
+    add_expense(3, String::from("test"), String::from("test"), 60.00, &mut expenses);
+    add_expense(4, String::from("test"), String::from("test"), 60.00, &mut expenses);
+
+    delete_expense(5, &mut expenses);
 
     for expense in &expenses{
-        println!("{} {} {}",expense.name,expense.date,expense.amount)
+        println!("{} {} {} {}",expense.id,expense.name,expense.date,expense.amount)
     }
     println!()
 }
