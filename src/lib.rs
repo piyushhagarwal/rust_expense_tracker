a7efa84d3979174242c7d7dd2e0c33a7efdc73b7
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-17 20:46:49 +0530

    completed add, delete and update functions with tests

--- src/lib.rs
+++ src/lib.rs
@@ -37,50 +37,43 @@ impl User{
         );
         self.account_balance -= amount;
     }
-}
 
+    pub fn delete_expense(&mut self,id: i32){
+        let mut index = 0;
+        for element in self.transactions.iter(){
+            if element.id == id {
+                break;
+            }
+            else{
+                index += 1;
+            }
+        }
+        if index < self.transactions.len() {
+            self.account_balance += self.transactions[index].amount;
+            self.transactions.remove(index);
 
-//add expense function 
-pub fn add_expense(id: i32, name: String, date: String, amount: f64, expenses_vector: &mut Vec<Expense>){
-    expenses_vector.push(Expense {id, name: String::from(name), date: String::from(date), amount: amount });
-
-
-}
-
-//delete expense function
-pub fn delete_expense(id: i32, expenses_vector: &mut Vec<Expense>){
-    let mut index = 0;
-    for element in expenses_vector.iter(){
-        if element.id == id {
-            break;
         }
         else{
-            index += 1;
+            println!("expense not found");
         }
     }
-    if index < expenses_vector.len() {
-        expenses_vector.remove(index);
-    }
-    else{
-        println!("expense not found");
-    }
-}
 
-//update expense function
-
-pub fn update_expense(id: i32, updated_name: String, updated_date: String, updated_amount: f64, expenses_vector: &mut Vec<Expense>){
-    for expense in expenses_vector.iter_mut(){
-        if id == expense.id {
-            expense.name = updated_name;
-            expense.date = updated_date;
-            expense.amount = updated_amount;
-            return;
+    pub fn update_expense(&mut self, id: i32, updated_name: String, updated_date: String, updated_amount: f64){
+        for expense in self.transactions.iter_mut(){
+            if id == expense.id {
+                self.account_balance += expense.amount;
+                expense.name = updated_name;
+                expense.date = updated_date;
+                expense.amount = updated_amount;
+                self.account_balance -= expense.amount;
+                return;
+            }
         }
+        println!("Element not found")
     }
-    println!("Element not found")
+
+
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -98,6 +91,28 @@ mod tests {
         let mut user1 = User::new("piyush".to_string());
         user1.add_balance(100.00);
         user1.add_expense(1, "movie".to_string(), "17/07/2023".to_string(), 50.00);
+        assert_eq!(user1.transactions[0].id, 1);
         assert_eq!(user1.account_balance,50.00);
+    }
+
+    #[test]
+    fn check_delete_expense(){
+        let mut user1 = User::new("piyush".to_string());
+        user1.add_balance(100.00);
+        user1.add_expense(1, "movie".to_string(), "17/07/2023".to_string(), 50.00);
+        user1.add_expense(2, "travel".to_string(), "18/07/2023".to_string(), 20.00);
+        user1.delete_expense(1);
+        assert_eq!(user1.account_balance,80.00);
+        assert_eq!(user1.transactions[0].id, 2);
+    }
+
+    #[test]
+    fn check_update_expense(){
+        let mut user1 = User::new("piyush".to_string());
+        user1.add_balance(100.00);
+        user1.add_expense(1, "movie".to_string(), "17/07/2023".to_string(), 50.00);
+        user1.update_expense(1,"traval".to_string(), "18/07/2023".to_string(), 60.00);
+        assert_eq!(user1.account_balance,40.00);
+        assert_eq!(user1.transactions[0].amount, 60.00);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use rust_expense_tracker::Expense;
-fn main() {
-    let mut expenses: Vec<Expense> = Vec::new();
-
-    rust_expense_tracker::add_expense(1, String::from("test"), String::from("test"), 60.00, &mut expenses);
-    rust_expense_tracker::add_expense(2, String::from("test"), String::from("test"), 60.00, &mut expenses);
-    rust_expense_tracker::add_expense(3, String::from("test"), String::from("test"), 60.00, &mut expenses);
-    rust_expense_tracker::add_expense(4, String::from("test"), String::from("test"), 60.00, &mut expenses);
-
-    rust_expense_tracker::delete_expense(4, &mut expenses);
-
-    rust_expense_tracker::update_expense(1, String::from("test1"), String::from("test1"), 80.00, &mut expenses);
-
-
-
-    for expense in &expenses{
-        println!("{} {} {} {}",expense.id,expense.name,expense.date,expense.amount)
-    }
-    println!()
+fn main(){
+    println!("hello world ")
 }
