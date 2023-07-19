03e0e650e5b6e064a4d6fa63569ac5d35c5acffb
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-19 10:49:57 +0530

    Completed basic file system functions with test cases

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,8 +1,14 @@
+use std::fs;
 use std::fs::{OpenOptions,File};
 use std::io::{self, prelude::*};
 
+pub fn open_file(file_path : &str) -> Result<(),io::Error> {
+    File::open(file_path)?;
+    Ok(())
+}
+
 pub fn create_file(file_path : &str) -> Result<(),io::Error> {
-    let _file = File::create(file_path)?;
+    File::create(file_path)?;
     Ok(())
 }
 
@@ -19,4 +25,65 @@ pub fn write_to_file(file_path : &str, data : &str) -> Result<(),io::Error> {
         .open(file_path)?;
     file.write_all(data.as_bytes())?;
     Ok(())
+}
+
+pub fn delete_file(file_path : &str) -> Result<(),io::Error> {
+    fs::remove_file(file_path)?;
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_file() {
+        let file_path = "test_file.txt";
+        assert!(create_file(file_path).is_ok());
+
+        // Clean up the file after the test is finished.
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_open_file() {
+        let file_path = "test_file.txt";
+        create_file(file_path).unwrap();
+        assert!(open_file(file_path).is_ok());
+
+        // Clean up the file after the test is finished.
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_read_file() {
+        let file_path = "test_file.txt";
+        let test_data = "hello world";
+        create_file(file_path).unwrap();
+        write_to_file(file_path, test_data).unwrap();
+        assert_eq!(read_file(file_path).unwrap(),test_data);
+        
+        // Clean up the file after the test is finished.
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_write_to_file(){
+        let file_path = "test_file.txt";
+        let test_data = "hello world";
+        create_file(file_path).unwrap();
+        assert!(write_to_file(file_path, test_data).is_ok());
+        
+        // Clean up the file after the test is finished.
+        fs::remove_file(file_path).unwrap();
+    }
+
+    #[test]
+    fn test_delete_file() {
+        let file_path = "test_file.txt";
+        create_file(file_path).unwrap();
+        assert!(delete_file(file_path).is_ok());
+    }
+    
 }
