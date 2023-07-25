0d723c98976891e8a8a8ebcbf849d88691f2989d
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-07-25 12:56:21 +0530

    created all apis

--- src/routes/expense.rs
+++ src/routes/expense.rs
@@ -1,35 +1,75 @@
-use axum::Json;
+use axum::{Json, extract::Path};
 use serde::{Serialize, Deserialize};
 
 use crate::filesystem;
 
+//Api to get a particular user by its id
+pub async fn get_user(Path(user_id): Path<i32>) -> Json<User>{
+    let file_name = format!("{}.json",user_id);
+    let json_string = filesystem::read_file(&file_name).unwrap();
+    let user_struct : User = serde_json::from_str(&json_string).unwrap();
+    Json(user_struct)
+}
+
 //Api to create User
-#[derive(Serialize,Deserialize)]
-pub struct BodyUser{
+#[derive(Deserialize)]
+pub struct BodyCreateUser{
     user_name: String,
     user_id: i32
 }
 
-pub async fn create_user(body: Json<BodyUser>) -> Json<BodyUser>{
+pub async fn create_user(body: Json<BodyCreateUser>) -> Json<User> {
     User::new(body.user_name.to_string(), body.user_id);
-    Json(BodyUser{
-        user_name: body.user_name.to_string(),
-        user_id: body.user_id
-    })
+    let file_name = format!("{}.json",body.user_id);
+    let json_string = filesystem::read_file(&file_name).unwrap();
+    let user_struct : User = serde_json::from_str(&json_string).unwrap();
+    Json(user_struct)
 }
 
 //Api to add add balance
 #[derive(Serialize,Deserialize)]
 pub struct BodyAddBalance{
-    user_id: i32,
     amount: f64
 }
-pub async fn add_balance(body: Json<BodyAddBalance>) -> Json<BodyAddBalance>{
-    User::add_balance(body.user_id, body.amount);
-    Json(BodyAddBalance{
-        user_id: body.user_id,
-        amount: body.amount
-    })
+pub async fn add_balance(Path(user_id): Path<i32>,body: Json<BodyAddBalance>) -> Json<User>{
+    User::add_balance(user_id, body.amount);
+    let file_name = format!("{}.json",user_id);
+    let json_string = filesystem::read_file(&file_name).unwrap();
+    let user_struct : User = serde_json::from_str(&json_string).unwrap();
+    Json(user_struct)
+}
+
+//Api for adding an expense 
+pub async fn add_expense(Path(user_id): Path<i32>,body: Json<Expense>) -> Json<User>{
+    User::add_expense(user_id, body.id, body.name.to_string(), body.date.to_string(), body.amount);
+    let file_name = format!("{}.json",user_id);
+    let json_string = filesystem::read_file(&file_name).unwrap();
+    let user_struct : User = serde_json::from_str(&json_string).unwrap();
+    Json(user_struct)
+}
+
+//Api for deleting an expense
+
+#[derive(Deserialize)]
+pub struct BodyDeleteExpense{
+    id: i32
+}
+
+pub async fn delete_expense(Path(user_id): Path<i32>,body: Json<BodyDeleteExpense>) -> Json<User>{
+    User::delete_expense(user_id, body.id);
+    let file_name = format!("{}.json",user_id);
+    let json_string = filesystem::read_file(&file_name).unwrap();
+    let user_struct : User = serde_json::from_str(&json_string).unwrap();
+    Json(user_struct)
+}
+
+//Api for updating an expense 
+pub async fn update_expense(Path(user_id): Path<i32>,body: Json<Expense>) -> Json<User>{
+    User::update_expense(user_id, body.id, body.name.to_string(), body.date.to_string(), body.amount);
+    let file_name = format!("{}.json",user_id);
+    let json_string = filesystem::read_file(&file_name).unwrap();
+    let user_struct : User = serde_json::from_str(&json_string).unwrap();
+    Json(user_struct)
 }
 
 #[derive(Serialize, Deserialize, Debug)]
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,13 @@
-use axum::{Router, routing::{get, post}};
+use axum::{Router, routing::{get, post, delete, patch}};
 mod expense;
 
 pub fn create_routes() -> Router<>{
     Router::new()
         .route("/createuser", post(expense::create_user))
-        .route("/addbalance", post(expense::add_balance))
+        .route("/:user_id/addbalance", post(expense::add_balance))
+        .route("/:user_id", get(expense::get_user))
+        .route("/:user_id/addexpense", post(expense::add_expense))
+        .route("/:user_id/deleteexpense", delete(expense::delete_expense))
+        .route("/:user_id/updateexpense", patch(expense::update_expense))
 }
 
